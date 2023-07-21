720839c654985c7e28c8fd2ef768fae56c9bdf25
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-07-21 22:31:37 +0100

    Add files via upload

---
+++ src/components/DrawerNotification.jsx
@@ -0,0 +1,110 @@
+// import React from "react";
+// import { MdOutlineNotifications } from "react-icons/md";
+
+// import {
+//   Drawer,
+//   Typography,
+//   IconButton,
+// } from "@material-tailwind/react";
+// import { XMarkIcon } from "@heroicons/react/24/outline";
+// import { AiFillApple } from "react-icons/ai";
+
+// const DrawerNotification = () => {
+  // const [openRight, setOpenRight] = React.useState(false);
+  // const openDrawerRight = () => setOpenRight(true);
+  // const closeDrawerRight = () => setOpenRight(false);
+  // return (
+    // <>
+      {/* <a onClick={openDrawerRight} className="opacity-60 " href="#">
+        <MdOutlineNotifications style={{ fontSize: "20px" }} />
+      </a> */}
+
+      {/* <Drawer
+        placement="right"
+        open={openRight}
+        onClose={closeDrawerRight}
+        className="p-4"
+        
+      >
+        <div className="mb-6 flex items-center justify-between" >
+          <Typography variant="h5" color="blue-gray">
+            Notifications
+          </Typography>
+          <IconButton
+            variant="text"
+            color="blue-gray"
+            onClick={closeDrawerRight}
+          >
+            <XMarkIcon strokeWidth={2} className="h-5 w-5" />
+          </IconButton>
+
+          
+        </div>
+
+    <div >
+    
+
+      <span className="list_notification">
+        <div className="icon_list_notification">
+            <AiFillApple className="icon_notification"/> 
+        </div>
+        <div className="list_notification_chidren">
+            <h4>Un condidat a ete passe un ...</h4>
+            <small>4m ago</small>
+        </div>
+      </span>
+      <span className="list_notification">
+        <div className="icon_list_notification">
+            <AiFillApple className="icon_notification"/> 
+        </div>
+        <div className="list_notification_chidren">
+            <h4>Nouveau inscription</h4>
+            <small>1:23 AM</small>
+        </div>
+      </span>
+      <span className="list_notification">
+        <div className="icon_list_notification">
+            <AiFillApple className="icon_notification"/> 
+        </div>
+        <div className="list_notification_chidren">
+            <h4>Un condidat a ete passe un ...</h4>
+            <small>4m ago</small>
+        </div>
+      </span>
+      <span className="list_notification">
+        <div className="icon_list_notification">
+            <AiFillApple className="icon_notification"/> 
+        </div>
+        <div className="list_notification_chidren">
+            <h4> Une message de Hamid</h4>
+            <small>Yesterday 12:39 AM</small>
+        </div>
+      </span>
+
+    </div>
+      </Drawer> */}
+
+
+
+    // </>
+  // );
+// };
+
+// export default DrawerNotification;
+
+
+
+
+// =========================================
+
+
+
+const DrawerNotification = () => {
+  return (
+    <>
+    dd
+    </>
+  );
+};
+
+export default DrawerNotification;
---
+++ src/pages/Support.jsx
@@ -0,0 +1,5 @@
+const Setting = () => {
+  return  <div className="p-4"> Setting</div>;
+};
+
+export default Setting;
