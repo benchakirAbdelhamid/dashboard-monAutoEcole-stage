7433e311448d30f9d3212b41469a4305a12ffb3f
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-08-02 19:23:53 +0100

    Delete src directory

--- src/pages/RolesPermissions.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const RolesPermissions = () => {
-  return <div className="p-4">
-    <h2 className="title">RolesPermissions</h2> </div>;
-};
-
-export default RolesPermissions;
