9afe959aab8e3e0a93b40d21a041424a6b43d091
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-08-02 18:44:13 +0100

    Delete src directory

--- src/pages/Formations.jsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-
-export default function Formation() {
-  return <div className="p-4">formation</div>;
-}
--- src/pages/ProfilePage.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from "react";
-
-const FileManager = () => {
-  return <div className="p-4">File Manager</div>;
-};
-
-export default FileManager;
