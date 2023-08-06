8e89a2a599f472ef2e1bf432010e49d583887a07
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-08-06 01:42:03 +0100

    Add files via upload

---
+++ src/pages/RolesPermissions.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const RolesPermissions = () => {
+  return <div className="p-4">
+    <h2 className="title">RolesPermissions</h2> </div>;
+};
+
+export default RolesPermissions;
