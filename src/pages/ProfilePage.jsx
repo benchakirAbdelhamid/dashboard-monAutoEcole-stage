99cadd3e13b708a558dbffa33316c05f509ffdea
Software Developer (Full Stack & Web Development) ' React Laravel MongoDB Developer <100100319+benchakirAbdelhamid@users.noreply.github.com>
2023-09-18 15:40:03 +0100

    Add files via upload

---
+++ src/pages/ProfilePage.jsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const ProfilePage = () => {
+  return (
+    <div className="p-4">
+      <h2 className=" title"> Profile Page </h2>
+    </div>
+  );
+};
+
+export default ProfilePage;
